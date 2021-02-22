5c75cc421524ea63a06515f204475f3d6cd6da82
Parker Smith <parkerbsmith@gmail.com>
2021-02-21 23:06:27 -0500

    Added get-observations lambda

---
+++ lambda/get-observations/index.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+
+const AWS = require("aws-sdk");
+const pino = require('pino');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+
+AWS.config.update({
+  region: "us-east-1"
+});
+
+async function getObservations(location, datetime) {
+  try {
+    if (!location.startsWith('https')) {
+      let newLocation = `https://api.weather.gov/stations/${location}`;
+      logger.debug(`setting location to <${newLocation}>`)
+      location = newLocation;
+    }
+
+    datetime = datetime.toISOString();
+
+    logger.info(`getting observations for <${location}> at <${datetime}>`);
+
+    let docClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+      "TableName": "weather-data",
+      "KeyConditionExpression": "#locationid = :locationid AND #observationtime < :observationtime",
+      "ExpressionAttributeValues": {
+        ":locationid": location,
+        ":observationtime": datetime
+      },
+      "ExpressionAttributeNames": {
+        "#locationid": "locationid",
+        "#observationtime": "observationtime"
+      },
+      "ScanIndexForward": true,
+      "Limit": 50
+    };
+
+    let data = await docClient.query(params).promise();
+    logger.info(`got back ${data.Count} (${data.ScannedCount} scanned)`);
+
+    return data;
+  }
+  catch (error) {
+    logger.error('failed to get observations: ' + error)
+  }
+}
+
+async function main(location, datetime) {
+  let records = await getObservations(location, datetime);
+  return records;
+}
+
+exports.handler = async function (request) {
+  let body = {};
+  try {
+    body = JSON.parse(request.body);
+  }
+  catch (error) {
+    logger.warn(`couldn't parse request body: ${request}`);
+  }
+
+  let location = body.location || 'KIND';
+  let datetime = body.datetime || new Date();
+
+  return await main(location, datetime);
+}
