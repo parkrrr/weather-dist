7bf7bc9837bb380cdb92d427ea9aeaf3efd7273f
Parker Smith <parkerbsmith@gmail.com>
2024-01-13 02:03:58 -0500

    Resolve build warnings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '/weather/',
+	build: {
+		outDir: './dist/weather'
+	},
 	plugins: [preact()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		}
+	}
 });
