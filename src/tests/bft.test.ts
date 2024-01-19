511e9ae76a7a444342117822ecfc76338f7b2344
Parker Smith <parkerbsmith@gmail.com>
2024-01-18 21:20:18 -0500

    proofread much

--- src/tests/bft.test.ts
+++ src/tests/bft.test.ts
@@ -33,7 +33,7 @@ test('Render Chart', async ({ page, browserName  }) => {
   expect(dataPointCount).toBeGreaterThanOrEqual(20);
   
   await page.screenshot({
-    path: `./screenshots/'
+    path: './screenshots/',
     fullPage: true
   });
 });
