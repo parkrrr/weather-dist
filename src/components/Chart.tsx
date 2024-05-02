53094df001903ea3127fa91d4cfb9258b1dbbdeb
Parker Smith <parkerbsmith@gmail.com>
2024-05-01 22:31:04 -0400

    wip

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -10,22 +10,41 @@ export function Chart(props: { view: View, observations: ObservationViewModel[] 
 
   const pathCommands = props.observations.map((o, i) => {
     const x = i / (props.observations.length - 1);
-    const y = 1 - (o.value - minimum) / (maximum - minimum);
+    const y =  (o.value - minimum) / (maximum - minimum);
     return `${i === 0 ? 'M' : 'L'} ${x * 100} ${y * 100}`;
   }).join(' ');
 
   const points = props.observations.map((o, i) => {
     const x = i / (props.observations.length - 1);
-    const y = 1 - (o.value - minimum) / (maximum - minimum);
+    const y =  (o.value - minimum) / (maximum - minimum);
     return { x: x * 100, y: y * 100 };
+  });
+
+  // generate 10 numbers in between `minimum` and `maximum`
+  const spacing = (maximum - minimum) / 10;
+  const gridLinesRange = Array.from({ length: 10 }, (_, i) => { return spacing * i + minimum; });
+
+
+  const gridLines = gridLinesRange.map((o, i) => {
+    const x = o;
+    return { x1: x * 100, y1: 0, x2: x * 100, y2: 100 };
+    
   });
 
   return (
     <svg id="chart" viewBox="-5 -5 110 110" xmlns="http://www.w3.org/2000/svg">
-      <g className={style.point}>
-        {points.map(p => <circle cx={p.x} cy={p.y} r="0.2" />)}
+      <g>
+        {gridLines.map(p => 
+        <>
+        <line className={style.gridline} x1={p.x1} y1={p.y1} x2={p.x2} y2={p.y2} stroke-dasharray="1 1" />
+        <line className={style.gridline} x1={p.y1} y1={p.x1} x2={p.y2} y2={p.x2} stroke-dasharray="1 1" />
+        </>
+        )}     
       </g>
-      <path className={style.line} d={pathCommands} fill="transparent" stroke-width="0.5" fill-opacity="0.5" />
+      <g className={style.point}>
+        {points.map(p => <circle cx={p.x} cy={p.y} r="0.5" />)}
+      </g>
+      <path className={style.line} d={pathCommands} fill="transparent" stroke-width="1" fill-opacity="0.5" />
     </svg>
   );
 
