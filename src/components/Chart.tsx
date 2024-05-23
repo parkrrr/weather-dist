29c8361916f1ccb1d7af4ab70a018c24cd173d8d
Parker Smith <parkerbsmith@gmail.com>
2024-05-23 00:20:45 -0400

    Spacing

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -54,7 +54,7 @@ export function Chart(props: { view: View, observations: ObservationViewModel[] 
   });
 
   return (
-    <svg id="chart" viewBox="-5 -5 110 130" xmlns="http://www.w3.org/2000/svg">
+    <svg id="chart" viewBox="0 -5 102 125" xmlns="http://www.w3.org/2000/svg">
       <g>
         {verticalGridLines.map(p => <line className={style.gridline} x1={p.x1} y1={p.y1} x2={p.x2} y2={p.y2} stroke-dasharray="1 1" />)}
         {horizontalGridLines.map(p => <line className={style.gridline} x1={p.x1} y1={p.y1} x2={p.x2} y2={p.y2} stroke-dasharray="1 1" />)}
@@ -62,7 +62,7 @@ export function Chart(props: { view: View, observations: ObservationViewModel[] 
       <g>
         {yAxisLabels.map((y, i) => <text x={0} y={y} className={style['label-y']}>{valueFormatter(gridLinesRange[i])}</text>)}
         {xAxisLabels.map((x, i) =>
-          <foreignObject x={x} y={105} style="overflow: visible;">
+          <foreignObject x={x-1} y={101} style="overflow: visible;">
             <span className={style['label-x']}>{dateFormatter.format(dateLinesRange[i])}</span>
           </foreignObject>
         )}
